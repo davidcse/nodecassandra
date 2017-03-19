923ae8d18b5ca17e9e7cee33ab0b12f181ce4d73
David Lin <david.lin.cse@gmail.com>
2017-03-19 14:04:06 -0400

    deposit and retrieve works

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var cassandra = require('cassandra-driver');
 var app = express();
 
 // view engine setup
@@ -22,8 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var index = require('./routes/index');
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,87 @@
 var express = require('express');
 var router = express.Router();
+var cassandra = require('cassandra-driver');
+var fs = require('fs');
 
-/* GET home page. */
+//CONNECT TO CASSANDRA FOR USE FOR THE INDEX MODULE
+var client = new cassandra.Client({contactPoints:['127.0.0.1']});
+client.connect(function(err,result){
+  if(err){
+    console.log(err)
+  }
+  console.log("cassandra is connected");
+});
+
+/* QUERIES TO CASSANDRA USING DB CLIENT */
+var query_insert_file = "INSERT INTO hw4.imgs (id, filename,contents) VALUES(?,?,textAsBlob(?));";
+var query_retrieve_file = "SELECT blobAsText(contents) as contents FROM hw4.imgs WHERE filename = ? ALLOW FILTERING;";
+
+//SEND USER TO HOMEPAGE
 router.get('/', function(req, res, next) {
+  console.log("encountered user at index /");
   res.render('index', { title: 'Express' });
+});
+
+//DEPOSIT USER FILE CONTENTS INTO CASSANDRA DB
+router.post('/deposit',function(req,res,next){
+  console.log('depositing file:' + req.body.filename + "\tcontents length:" + req.body.contents);
+  //check if provided necessary parameters in post body
+  if(!req.body.filename || !req.body.contents){
+    return res.json({'status':'ERROR', 'message': 'missing necessary parameters'});
+  }
+
+  //generate unique id for this insert statement
+  var id = cassandra.types.uuid();
+  // wrap binary data into json, then convert to text.
+  var contentWrapper = JSON.stringify({
+    'binarydata' : req.body.contents
+  });
+  //tell client to execute insert statement, with injected parameters.
+  client.execute(query_insert_file,[id, req.body.filename, contentWrapper],function(err,result){
+    if(err){
+      console.log(err);
+      return res.json({'status':'ERROR', 'message': 'deposit failed'});
+    }
+    console.log('finished successful depositing');
+    return res.json({'status':'OK','message':'deposit  successful'});
+  })
+});
+
+
+
+//RETRIEVE THE SPECIFIC FILE FROM CASSANDRA DB. FIRST MATCHING ROW ONLY.
+router.post('/retrieve',function(req,res,next){
+  console.log('retrieving the file: ' + req.body.filename);
+  //check if params we need are provided.
+  if(!req.body.filename){
+    return res.json({'status':'ERROR','message': 'missing necessary parameters'});
+  }
+  // else query db for filename
+  client.execute(query_retrieve_file,[req.body.filename],function(err,result){
+    if(err){
+      console.log(err);
+      return res.json({'status':'ERROR', 'message': 'retrieval failed'});
+    }
+    //Successfully retrieved a result, process first matching row.
+    var retrievedRow = result.rows[0];
+    var jsoContents = JSON.parse(retrievedRow.contents); //converted from blob to text in select query.
+    const buf = Buffer.from(jsoContents.binarydata);
+    var filepath = "/data/"+req.body.filename;
+
+    //log statements
+    console.log("extracted retrievedRow from db: " + JSON.stringify(retrievedRow));
+    console.log("extracted jsoContents from db: " + JSON.stringify(jsoContents));
+    console.log('writing to file:' + filepath);
+    console.log("with contents : " + buf.toString());
+    fs.writeFile(filepath, buf, 'binary', function(err){
+      if(err){
+        console.log(err);
+        return res.json({"status":"ERROR", "message": "Server could not write to file"})
+      }
+      console.log('finished writing to file, sent to client');
+      return res.sendFile(filepath);
+    });
+  });
 });
 
 module.exports = router;
